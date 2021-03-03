75ee0bc9784ccc03e437a1495f435028214796d1
Miller <nmiller2379@gmail.com>
2021-03-03 10:09:20 -0700

    server working

---
+++ models/workout.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema ({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Please enter an exercise type."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise name."
+            },
+            duration: {
+                type: Number,
+                required: "Please enter an exercise duration."
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+        },
+    ],
+},
+{ toJSON: { virtuals: true } }
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false });
+
+// app.use(require("./routes/apiRoutes"));
+// app.use(require("./routes/htmlRoutes"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
