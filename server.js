d77177e025cf4fa11d0b9278e6bdef6b0254070f
Miller <nmiller2379@gmail.com>
2021-03-04 17:34:29 -0700

    deploying

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false  
+ }
+ );
 
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
